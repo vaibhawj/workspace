3587d4231f44474f225e702e966b80f81f7226ec
Vaibhaw Jaiswal <vj.vaibhaw@gmail.com>
2016-11-06 02:37:58 +0530

    Cyclic barrier

--- UseCases/src/codes/MyCyclicBarrier.java
+++ UseCases/src/codes/MyCyclicBarrier.java
@@ -2,4 +2,59 @@ package codes;
 
 public class MyCyclicBarrier {
 
+	private int parties;
+
+	private volatile int count;
+
+	private volatile boolean barrierBroken;
+
+	private Runnable barrierCommand;
+
+	private volatile boolean barrierCommandExecuted;
+
+	public MyCyclicBarrier(int parties) {
+		this.parties = parties;
+		this.count = parties;
+	}
+
+	public MyCyclicBarrier(int parties, Runnable barrierAction) {
+		this.parties = parties;
+		this.count = parties;
+		this.barrierCommand = barrierAction;
+	}
+
+	public void await() throws BarrierBrokenException {
+		this.count--;
+
+		while (this.count > 0) {
+			if (this.barrierBroken) {
+				this.barrierBroken = false;
+				throw new BarrierBrokenException();
+			}
+		}
+
+		if (!barrierCommandExecuted) {
+			barrierCommandExecuted = true;
+			barrierCommand.run();
+		}
+	}
+
+	public void reset() {
+		count = parties;
+		this.barrierBroken = true;
+	}
+
+	public int getNumberWaiting() {
+		return this.parties - this.count;
+	}
+
+	static class BarrierBrokenException extends Exception {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1340198090568923005L;
+
+	}
+
 }
---
+++ UseCases/src/codes/MyCyclicBarrierTester.java
@@ -0,0 +1,58 @@
+package codes;
+
+import codes.MyCyclicBarrier.BarrierBrokenException;
+
+public class MyCyclicBarrierTester {
+
+	public static void main(String[] args) {
+		MyCyclicBarrier cb = new MyCyclicBarrier(3, new BarrierAction());
+
+		Thread t1 = new Thread(new MyRunnable(cb), "T1");
+		Thread t2 = new Thread(new MyRunnable(cb), "T2");
+		Thread t3 = new Thread(new MyRunnable(cb), "T3");
+
+		t1.start();
+		t2.start();
+		t3.start();
+
+	}
+
+	static class BarrierAction implements Runnable {
+
+		@Override
+		public void run() {
+			System.out.println("Barrier Action executed by " + Thread.currentThread().getName());
+		}
+
+	}
+
+	static class MyRunnable implements Runnable {
+
+		private MyCyclicBarrier cb;
+
+		public MyRunnable(MyCyclicBarrier cb) {
+			this.cb = cb;
+		}
+
+		@Override
+		public void run() {
+			System.out.println(Thread.currentThread().getName() + " just started");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(Thread.currentThread().getName() + " reaching barrier");
+
+			try {
+				this.cb.await();
+			} catch (BarrierBrokenException e) {
+				e.printStackTrace();
+			}
+
+			System.out.println(Thread.currentThread().getName() + " moving ahead");
+
+		}
+
+	}
+}
