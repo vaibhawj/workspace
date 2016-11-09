34fe7d496d068266a6b452ce317151f0c2df20f9
Vaibhaw Jaiswal <vj.vaibhaw@gmail.com>
2016-11-09 07:18:00 +0530

    Singleton enum

---
+++ Workshop/src/day4/SingletonEnum.java
@@ -0,0 +1,21 @@
+package day4;
+
+public enum SingletonEnum {
+
+	INSTANCE;
+
+	public void fun() {
+
+	}
+
+	public static void main(String[] args) {
+
+		SingletonEnum obj = SingletonEnum.INSTANCE;
+
+		SingletonEnum obj2 = SingletonEnum.INSTANCE;
+
+		System.out.println(obj == obj2);
+
+	}
+
+}
