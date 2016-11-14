c1d15f412e74a531d19b0a70f7627bc039ce6c47
Vaibhaw Kumar <vj.vaibhaw@gmail.com>
2016-11-14 09:37:36 +0530

    day8

---
+++ Workshop/src/day8/newFeatures/ForkJoinDemo.java
@@ -0,0 +1,13 @@
+package day8.newFeatures;
+
+import java.util.concurrent.ForkJoinPool;
+
+public class ForkJoinDemo {
+
+	public static void main(String[] args) {
+
+		ForkJoinPool pool = new ForkJoinPool(2);
+
+	}
+
+}
