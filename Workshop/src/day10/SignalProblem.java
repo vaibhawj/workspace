a6926a5c6aceba506a35e156a6047bc6b712db7b
Vaibhaw Kumar <vj.vaibhaw@gmail.com>
2016-11-16 18:42:59 +0530

    SignalProblem

--- Workshop/src/day10/SignalProblem.java
+++ Workshop/src/day10/SignalProblem.java
@@ -1,38 +1,89 @@
 package day10;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class SignalProblem {
 
+	static SignalProblem s;
+	Signal signal1;
+	Signal signal2;
+	Signal signal3;
+	Signal signal4;
+
 	public static void main(String[] args) {
+		s = new SignalProblem();
+		s.letTheFunBegin();
 
-		Thread signalThread1 = new Thread(new Signal(new RedState()), "S1");
-		Thread signalThread2 = new Thread(new Signal(new GreenState()), "S2");
-		Thread signalThread3 = new Thread(new Signal(new RedState()), "S3");
-		Thread signalThread4 = new Thread(new Signal(new RedState()), "S4");
+	}
 
-		signalThread1.start();
+	private void letTheFunBegin() {
+		Lock lock = new ReentrantLock(true);
+		Condition tripCondition = lock.newCondition();
+		signal1 = new Signal(new GreenState(), lock, tripCondition);
+		signal2 = new Signal(new RedState(), lock, tripCondition);
+		signal3 = new Signal(new RedState(), lock, tripCondition);
+		signal4 = new Signal(new RedState(), lock, tripCondition);
+
+		Thread signalThread1 = new Thread(signal1, "S1");
+		Thread signalThread2 = new Thread(signal2, "S2");
+		Thread signalThread3 = new Thread(signal3, "S3");
+		Thread signalThread4 = new Thread(signal4, "S4");
+
 		signalThread2.start();
 		signalThread3.start();
 		signalThread4.start();
+		signalThread1.start();
+	}
 
+	void printStatus() {
+		System.out.println("S1 is " + signal1.state);
+		System.out.println("S2 is " + signal2.state);
+		System.out.println("S3 is " + signal3.state);
+		System.out.println("S4 is " + signal4.state);
+
+		System.out.println("************************");
 	}
 
 	static class Signal implements Runnable {
 
 		private State state;
+		private Lock lock;
+		private int numOfIterations;
+		private Condition condition;
 
-		public Signal(State state) {
+		public Signal(State state, Lock lock, Condition tripCondition) {
 			this.state = state;
+			this.lock = lock;
+			this.condition = tripCondition;
 		}
 
 		@Override
 		public void run() {
 
-			if ("Red".equals(state)) {
-				state.switchState(this);
+			while (numOfIterations <= 4) {
+				numOfIterations++;
+				lock.lock();
+				if ("Red".equals(state.toString())) {
+					try {
+						condition.await();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+
+					state.switchState(this);
+					s.printStatus();
+				} else {
+					state.switchState(this);
+					condition.signal();
+				}
+
+				lock.unlock();
+
 			}
 
 		}
-
 	}
 
 	interface State {
