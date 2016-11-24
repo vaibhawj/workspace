ae14ba193cdee7c24207a8f1ada61bf13beb6efb
Vaibhaw Kumar <vj.vaibhaw@gmail.com>
2016-11-24 12:32:00 +0530

    day15 thing

---
+++ Workshop/src/day15/MyThread.java
@@ -0,0 +1,22 @@
+package day15;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MyThread extends Thread implements Callable<Long> {
+
+	public static void main(String[] args) {
+		MyThread t = new MyThread();
+
+		ExecutorService e = Executors.newSingleThreadExecutor();
+		e.submit(t);
+	}
+
+	@Override
+	public Long call() throws Exception {
+
+		return 12l;
+	}
+
+}
---
+++ Workshop/src/day15/sort/QuickSort.java
@@ -0,0 +1,18 @@
+package day15.sort;
+
+public class QuickSort {
+
+	public static void main(String[] args) {
+		int[] unSortedArr = { 10, 6, 4, 8, 3, 9, 1 };
+
+		QuickSort q = new QuickSort();
+		int[] sortedArr = q.sort(unSortedArr);
+
+	}
+
+	private int[] sort(int[] unSortedArr) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
