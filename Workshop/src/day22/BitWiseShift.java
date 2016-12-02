0fe9572d91c46458fe6b4a07d24f3db8e2c422ea
Vaibhaw Kumar <vj.vaibhaw@gmail.com>
2016-12-02 16:40:17 +0530

    bitwise

--- Workshop/src/day22/BitWiseShift.java
+++ Workshop/src/day22/BitWiseShift.java
@@ -2,31 +2,26 @@ package day22;
 
 public class BitWiseShift {
 
-	static final int HASH_BITS = 0x7fffffff;
-
 	public static void main(String[] args) {
-		// 11 -> 1 =1
-		System.out.println(3 >> 1);
 
-		// 10 -> 1 =1
-		System.out.println(2 >> 1);
+		int i = -2;
+		System.out.println(i + " << " + (i << 1));
 
-		// 100 -> 10 =2
-		System.out.println(4 >> 1);
+		i = -2;
+		System.out.println(i + " >> " + (i >> 1));
 
-		// 11 -> 110 =6
-		System.out.println(3 << 1);
+		i = 2;
+		System.out.println(i + " << " + (i << 1));
 
-		// 10 -> 100 =4
-		System.out.println(2 << 1);
+		i = 2;
+		System.out.println(i + " >> " + (i >> 1));
 
-		// unsigned shift
-		// TODO
+		i = -2;
+		System.out.println(i + " >>> " + (i >>> 1));
 
-	}
+		i = 2;
+		System.out.println(i + " >>> " + (i >>> 1));
 
-	static final int spread(int h) {
-		return (h ^ (h >>> 16)) & HASH_BITS;
 	}
 
 }
