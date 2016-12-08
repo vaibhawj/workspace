afe5db7c71320681d8cec24d5a2a5c4c30d07194
Vaibhaw Kumar <vj.vaibhaw@gmail.com>
2016-12-08 12:52:35 +0530

    merge two sorted linkedlist recursively

--- ProblemSolving/src/ds/MergeTwoSortedLinkedList.java
+++ ProblemSolving/src/ds/MergeTwoSortedLinkedList.java
@@ -9,21 +9,41 @@ public class MergeTwoSortedLinkedList extends AbstractMyLinkedList {
 
 		Node h2 = new Node(2);
 		h2.next = new Node(3);
-		h2.next.next = new Node(20);
+		h2.next.next = new Node(12);
+		h2.next.next.next = new Node(20);
 
 		print(h1);
 		print(h2);
-		Node h3 = merge(h1, h2);
-		print(h3);
+		System.out.println("Merged");
+		print(merge(h1, h2, null, null));
 
 	}
 
-	private static Node merge(Node h1, Node h2) {
+	private static Node merge(Node h1, Node h2, Node mergedHead, Node prevNode) {
 
-		Node h3 = null;
+		if (null == h1 || null == h2) {
+			return mergedHead;
+		}
 
-		return h3;
+		int minValue;
+		if (h1.value < h2.value) {
+			minValue = h1.value;
+			h1 = h1.next;
+		} else {
+			minValue = h2.value;
+			h2 = h2.next;
+		}
+
+		if (mergedHead == null) {
+			mergedHead = new Node(minValue);
+			prevNode = mergedHead;
+		} else {
+			Node currNode = new Node(minValue);
+			prevNode.next = currNode;
+			prevNode = currNode;
+		}
+
+		return merge(h1, h2, mergedHead, prevNode);
 
 	}
-
 }
